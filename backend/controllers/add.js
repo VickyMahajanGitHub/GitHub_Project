3398d80f850114bf92a6e5ad4ff94fc80825b664
Vicky Mahajan <110395736+VickyMahajanGitHub@users.noreply.github.com>
2025-06-16 16:59:25 +0530

    add logic

--- backend/controllers/add.js
+++ backend/controllers/add.js
@@ -1,5 +1,20 @@
-async function addRepo() {
-    console.log("Add commnand called")
+const fs= require("fs/promises");
+const path = require("path");
+
+async function addRepo(filePath) {
+    const repoPath = path.resolve(process.cwd(), ".vicky_git");
+    const stagingPath = path.join(repoPath, "staging");
+
+    try {
+        await fs.mkdir(stagingPath, { recursive: true });
+        const fileName = path.basename(filePath);
+        await fs.copyFile(filePath, path.join(stagingPath, fileName));
+        console.log(`File ${fileName} added to staging area.`);
+
+    }
+    catch (error) {
+        console.error("Error adding file to staging area:", error);
+    }
 }
 
 module.exports = { addRepo };
--- backend/index.js
+++ backend/index.js
@@ -10,26 +10,44 @@ const { revertRepo } = require("./controllers/revert");
 
 yargs(hideBin(process.argv))
   .command("init", "Initialize the project", {}, initRepo)
-  .command("add <file>", "Add a file to the project", (yargs) => {
-    yargs.positional("file", {
-      describe: "The file to add to staging area",
-      type: "string"
-    });
-  }, addRepo)
-  .command("commit <message>", "Commit changes to the project", (yargs) => {
-    yargs.positional("message", {
-      describe: "The commit message",
-      type: "string"
-    });
-  }, commitRepo)
-  .command("push","Push changes to the remote repository",{},pushRepo)
-  .command("pull", "Pull changes from the remote repository", {},pullRepo)
-  .command("revert <commitID>", "Revert changes to a specific commit", (yargs) => {
-    yargs.positional("commitID", {
-      describe: "The commit ID to revert to",
-      type: "string"
-    });
-  }, revertRepo)
+  .command(
+    "add <file>",
+    "Add a file to the project",
+    (yargs) => {
+      yargs.positional("file", {
+        describe: "The file to add to staging area",
+        type: "string",
+      });
+    },
+    (argv) => {
+      addRepo(argv.file);
+    }
+  )
+
+  .command(
+    "commit <message>",
+    "Commit changes to the project",
+    (yargs) => {
+      yargs.positional("message", {
+        describe: "The commit message",
+        type: "string",
+      });
+    },
+    commitRepo
+  )
+  .command("push", "Push changes to the remote repository", {}, pushRepo)
+  .command("pull", "Pull changes from the remote repository", {}, pullRepo)
+  .command(
+    "revert <commitID>",
+    "Revert changes to a specific commit",
+    (yargs) => {
+      yargs.positional("commitID", {
+        describe: "The commit ID to revert to",
+        type: "string",
+      });
+    },
+    revertRepo
+  )
   .demandCommand(1, "You need to specify a command")
   .strict()
   .help().argv;
