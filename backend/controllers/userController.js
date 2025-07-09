cb046d26be75db3f819eec21510a555a0d86a297
Vicky Mahajan <110395736+VickyMahajanGitHub@users.noreply.github.com>
2025-07-09 17:26:03 +0530

    Schemas and routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,34 @@
+const getAllUsers = (req,res) => {
+    res.send("All users fetched!");
+};
+
+const signup = (req,res) => {
+    res.send("User signed up!");
+};
+
+const login = (req,res) => {
+    res.send("User logged in!");
+};
+
+// CRUD Operation
+
+const getUserProfile = (req,res) => {
+    res.send("User profile fetched!");
+};
+
+const updateUserProfile = (req,res) => {
+    res.send("User profile updated!");
+};
+
+const deleteUserProfile = (req,res) => {
+    res.send("User profile deleted!");
+};
+
+module.exports = {
+    getAllUsers,
+    signup,
+    login,
+    getUserProfile,
+    updateUserProfile,
+    deleteUserProfile,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const yargs = require("yargs");
 const { hideBin } = require("yargs/helpers");
 
@@ -7,8 +15,13 @@ const { commitRepo } = require("./controllers/commit");
 const { pushRepo } = require("./controllers/push");
 const { pullRepo } = require("./controllers/pull");
 const { revertRepo } = require("./controllers/revert");
+const mainRouter =require("./routes/main.router");
+
+dotenv.config();
 
 yargs(hideBin(process.argv))
+  .command("start", "Start the project", {}, startServer)
+  
   .command("init", "Initialize the project", {}, initRepo)
   .command(
     "add <file>",
@@ -55,3 +68,62 @@ yargs(hideBin(process.argv))
   .demandCommand(1, "You need to specify a command")
   .strict()
   .help().argv;
+
+function startServer() {
+  const app = express();
+  const port = process.env.PORT || 5000;
+
+  app.use(bodyParser.json());
+  app.use(express.json());
+  
+
+  const mongoURI = process.env.MONGO_URI;
+  mongoose
+    .connect(mongoURI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
+
+  app.use(cors( {origin:"*"} )); // Allow all origins/domains for CORS
+
+  app.use("/", mainRouter);
+
+ 
+
+  let user = "Vicky";
+  const httpServer = http.createServer(app);
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "*", // Allow all origins for WebSocket connections
+      methods: ["GET", "POST"],
+    },
+  });
+
+
+  io.on("connection",(socket)=> {
+    socket.on("joinRoom",(userID) => {
+      user = userID;
+      console.log("=======")
+      console.log(`${user} has joined the room`);
+      console.log("=======")
+      socket.join(userID);
+    });
+  });
+
+  const db= mongoose.connection;
+  db.once("open", async () => {
+    console.log("MongoDB connection established successfully || CRUD operations"); // CRUD operations can be performed here
+    
+    // Emit an event to notify clients that the server is ready
+    io.emit("serverReady", { message: "Server is ready", user });
+  });
+
+  httpServer.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  
+
+}
