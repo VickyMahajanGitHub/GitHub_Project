f1611fa237521d0fa8fa31e2f83466972f7b2151
Vicky Mahajan <110395736+VickyMahajanGitHub@users.noreply.github.com>
2025-09-13 18:42:54 +0530

    Login.jsx change

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
 
         try {
             setLoading(true);
-            const res = await axios.post("http://localhost:5000/users/login", {
+            const res = await axios.post("https://githubproject-production.up.railway.app/users/login", {
                 email: email,
                 password: password,
             });
