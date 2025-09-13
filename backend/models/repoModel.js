abe7a9459009c14b5688fe240cd0e6ceca9e91a8
Vicky Mahajan <110395736+VickyMahajanGitHub@users.noreply.github.com>
2025-09-13 19:30:11 +0530

    correction

--- backend/models/repoModel.js
+++ backend/models/repoModel.js
@@ -1,43 +1,40 @@
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const RepositorySchema = new Schema ({
+const RepositorySchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     description: {
-        type: String,
+      type: String,
     },
     content: [
-        {
-            type:String,
-        },
+      {
+        type: String,
+      },
     ],
-    visibility:{
-        type: Boolean,
-
+    visibility: {
+      type: Boolean,
     },
-    Owner: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-        required: true
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     issues: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Issue"
-        },
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Issue",
+      },
     ],
-    
-
-},
-{
+  },
+  {
     timestamps: true,
-}
+  }
 );
 
-
 const Repository = mongoose.model("Repository", RepositorySchema);
-module.exports = Repository;    
+module.exports = Repository;
